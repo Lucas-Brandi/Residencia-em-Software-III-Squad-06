3f680338a643ef55105fa6134c3688897a2965f4
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-05-05 14:21:59 -0300

    fix: resolvendo um problema de importação do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubWebhookModule } from './webhook/github-webhook.module';
-// import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { RepositoriesModule } from './repositories/repositories.module';
 import { AnalysisRulesModule } from './analysis-rules/analysis-rules.module';
