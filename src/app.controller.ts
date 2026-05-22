7316bfd1078c9607166be1d74cc70455dbe7ae40
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-05-22 12:24:18 -0300

    fix: deixando pública a rota de app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Public } from './auth/decorators/public.decorator';
 
 @ApiTags('App')
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   @ApiOperation({ summary: 'Verificar status da API' })
   @ApiResponse({
